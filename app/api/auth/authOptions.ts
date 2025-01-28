51185bd37341b7d42e91e8ca989df98bdf74de46
sebaactis <sebaactis@gmail.com>
2025-01-28 11:50:51 -0300

    secret configuration

--- app/api/auth/authOptions.ts
+++ app/api/auth/authOptions.ts
@@ -2,6 +2,7 @@ import { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: AuthOptions = {
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         CredentialsProvider({
             name: "Credentials",
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: false,
+  },
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
   }
 };
 
